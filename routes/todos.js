2d3c7e5b7acdb9b375e0c1c7dad0139297984824
Allan Kenneth Rosal <allankennethrosal@gmail.com>
2024-05-07 00:57:19 +0800

    Modified /todos/:id route to advanced routing and setup basic middleware using .param

--- routes/todos.js
+++ routes/todos.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// retrieve all
+router.get('/all', (req, res) => {
   res.json({ message: "View All!" });
 })
 
-router.get('/view/:id', (req, res) => {
-  res.json({ message: "View!" });
-})
+// advanced routing
+router.route('/:id')
+  .get((req, res) => { // retrieve one
+    res.json({ message: "Retrieve!", todoId: req.params.todoId });
+  })
+  .post((req, res) => { // create
+    res.json({ message: "Create!", todoId: req.params.todoId });
+  })
+  .put((req, res) => { // update
+    res.json({ message: "Update!", todoId: req.params.todoId });
+  })
+  .delete((req, res) => { // delete
+    res.json({ message: "Delete!", todoId: req.params.todoId });
+  })
 
-router.post('/create', (req, res) => {
-  res.json({ message: "Create!" });
-})
-
-router.post('/update/:id', (req, res) => {
-  res.json({ message: "Update!" });
-})
-
-router.post('/delete/:id', (req, res) => {
-  res.json({ message: "Delete!" });
+// middleware
+router.param("id", (req, res, next, id) => {
+  req.params.todoId = id;
+  next();
 })
 
 module.exports = router;
