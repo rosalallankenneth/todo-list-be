4fce114a32796d77341fcb53e813f6ecdb80b3c6
Allan Kenneth Rosal <allankennethrosal@gmail.com>
2024-05-12 00:29:02 +0800

    Setup mongoose and env

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+// DATABASE CONNECTION
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+
+const db = mongoose.connection;
+
+db.on('error', error => console.log({ error }));
+db.once('connected', () => console.log('Database Connected'))
+
+// APP DEFAULT MIDDLEWARES
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
